b4934ee74f7795c7e1c0a01500cee4f8282d6aea
Simone B <simone.borelli@live.com>
2017-06-28 17:23:56 +0200

    Update test.js
    Finished and commented

--- test.js
+++ test.js
@@ -1,86 +1,75 @@
 const URL = "https://qa-engineer.herokuapp.com"
-
 let EC = protractor.ExpectedConditions;
+let timeout = 3000;
+
+
+// this function asserts that the quotes are all present in the page (famous and awesome): it's called twice
+let checkQuotes = (quotesType, QuotesToAssert) => {
+    let QuotesBlock = element(by.cssContainingText('body>ul>li', quotesType))
+    browser.wait(EC.visibilityOf(QuotesBlock), timeout, 'quotes display correctly');
+    let Quotes = QuotesBlock.$$('ul>li span:not(.score)').getText().then((texts) => {
+        expect(texts.length).toEqual(5, 'number of quotes is not 5 as expected');
+        texts.map((quote) => {
+            expect(QuotesToAssert.indexOf(quote)).not.toEqual(-1, `${quote} should not appear`);
+        });
+    });
+}
+
 
 describe('QA Engineer Test', function() {
+
+    const famousQuotesToAssert = [
+        `A classic is something that everyone wants to have read and nobody wants to read.`,
+        `If your life was a horse, you'd have to shoot it.`,
+        `You have taken yourself too seriously.`,
+        `Yes there is a lot of people doing a great job out there.`,
+        `You have the capacity to learn from mistakes. You'll learn a lot today.`
+    ]
+
+    const awesomeQuotesToAssert = [
+        `Beware of low-flying butterflies.`,
+        `Excellent time to become a missing person.`,
+        `I love deadlines. I love the whooshing sound they make as they fly by.`,
+        `Nothing so needs reforming as other people's habits.`,
+        `Do something unusual today. Pay a bill.`
+    ]
 
     beforeAll(function() {
         browser.ignoreSynchronization = true;
         browser.get(URL);
     });
 
-    it('reach code page via Enter button, then lists page', () => {
-        let timeout = 3000;
-
-        browser.wait(EC.textToBePresentInElement(browser.$('#enter'), "Enter"), timeout, 'Expect to find button with Enter as text');
-        browser.$('#enter').click();
-        browser.wait(EC.urlIs('https://qa-engineer.herokuapp.com/code'), timeout);
-        browser.$('input[type="hidden"]').getAttribute("value").then(function(valuex) {
-            browser.$('input[type="text"]').sendKeys(valuex);
-        });
-
-        browser.$('input[type="checkbox"]').click();
-
-        element(by.buttonText('Submit')).click();
-        browser.wait(EC.urlIs('https://qa-engineer.herokuapp.com/lists'), timeout);
-    });
-
 
     it('should verify quotes and categories', () => {
-
-        let timeout = 3000;
-
-        const famousQuotesToAssert = [
-            `A classic is something that everyone wants to have read and nobody wants to read.`,
-            `If your life was a horse, you'd have to shoot it.`,
-            `You have taken yourself too seriously.`,
-            `Yes there is a lot of people doing a great job out there.`,
-            `You have the capacity to learn from mistakes. You'll learn a lot today.`
-        ]
-
-        const awesomeQuotesToAssert = [
-            `Beware of low-flying butterflies.`,
-            `Excellent time to become a missing person.`,
-            `I love deadlines. I love the whooshing sound they make as they fly by.`,
-            `Nothing so needs reforming as other people's habits.`,
-            `Do something unusual today. Pay a bill.`
-        ]
-
-
-        let famousQuotesBlock = element(by.cssContainingText('body>ul>li', 'Famous Quotes'))
-        browser.wait(EC.visibilityOf(famousQuotesBlock), timeout);
-        let famousQuotes = famousQuotesBlock.$$('ul>li span:not(.score)').getText().then((texts) => {
-
-            expect(texts.length).toEqual(5);
-
-            texts.map((quote) => {
-                expect(famousQuotesToAssert.indexOf(quote)).not.toEqual(-1);
-            });
+        browser.wait(EC.textToBePresentInElement($('#enter'), "Enter"), timeout, 'Expect to find button with Enter as text');
+        $('#enter').click();
+        browser.wait(EC.urlIs('https://qa-engineer.herokuapp.com/code'), timeout, 'URL did not match the expcted one /code');
+        $('input[type="hidden"]').getAttribute("value").then(function(valuex) {
+            $('input[type="text"]').sendKeys(valuex);
         });
 
+        $('input[type="checkbox"]').click();
 
-        let awesomeQuotesBlock = element(by.cssContainingText('body>ul>li', 'Awesome Quotes'))
-        browser.wait(EC.visibilityOf(famousQuotesBlock), timeout);
-        let awesomeQuotes = awesomeQuotesBlock.$$('ul>li span:not(.score)').getText().then((texts) => {
+        element(by.buttonText('Submit')).click();
+        browser.wait(EC.urlIs('https://qa-engineer.herokuapp.com/lists'), timeout, 'URL did not match the expcted one /lists');
 
-            expect(texts.length).toEqual(5);
+        // calling the function which asserts the presence of all quotes in the correct groups
+        checkQuotes('Awesome Quotes', awesomeQuotesToAssert);
+        checkQuotes('Famous Quotes', famousQuotesToAssert);
 
-            texts.map((quote) => {
-                expect(awesomeQuotesToAssert.indexOf(quote)).not.toEqual(-1);
-            });
-        });
-
-
-        // checking the sum
+        // checking the sum of all scores next to each quote
         let scoresSumm = $$('span.score').getText().then(texts => {
             return texts.reduce((total, num) => { return (+total) + (+num) });
         })
 
+        // getting the value of Total Score as a number
         let totalScore = $('body').getText().then(text => {
             let matched = /Total score: (.*)/gm.exec(text)
             return +matched[1]
         })
 
+        // checking that total Scores actually matches the sum of all scores
+        expect(totalScore).toBe(scoresSumm, '<total score> does NOT match with the sum of scores')
 
     });
 
